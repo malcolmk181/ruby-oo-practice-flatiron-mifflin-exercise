34b36915a82f073e10812bad7a2c61bdc4593abe
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-06 12:06:07 -0400

    Done.

--- run.rb
+++ run.rb
@@ -4,7 +4,13 @@ require 'pry'
 
 
 #Test your code here
+larry = Manager.new("Larry", "Deliveries", 35)
+jimbo = Manager.new("Jim", "Sales", 28)
 
+larry.hire_employee("Mitchell", 40001)
+larry.hire_employee("Michelle", 40000)
+jimbo.hire_employee("Bob", 50000)
+jimbo.hire_employee("Taniya", 53000)
 
 binding.pry
 puts "done"
