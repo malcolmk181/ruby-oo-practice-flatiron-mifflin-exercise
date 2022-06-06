3007c13035ba6c147b40eec74acf7c561f082a31
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-06 12:06:01 -0400

    commit tree 64dac538de25a97f877bcc2c70b5def803f99c4d

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,35 @@
 class Employee
+    attr_accessor :name, :salary, :manager
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+    
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        self.class.all << self
+    end
+
+    def manager_name
+        @manager.name
+    end
+
+    def self.paid_over(amount)
+        all.select{|employee| employee.salary > amount }
+    end
+
+    def self.find_by_department(department)
+        all.find{|employee| employee.manager.department == department }
+    end
+
+    def tax_bracket
+        # going to assume the result SHOULD include the employee who called this method
+        
+        self.class.all.select{|employee| employee.salary.between?(self.salary - 1000, self.salary + 1000) }
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
+    attr_accessor :name, :department, :age
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+
+        self.class.all << self
+    end
+
+    def employees
+        Employee.all.select{|employee| employee.manager == self }
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        all.collect{|manager| manager.department }
+    end
+
+    def self.average_age
+        all.reduce(0) {|sum, manager| sum + manager.age } / all.count.to_f
+    end
 end
